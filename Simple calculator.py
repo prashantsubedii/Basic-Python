32e50777b918d1cb3a63d95d72d04ec1939f4319
Prashant Subedi <179603709+prashantsubedii@users.noreply.github.com>
2026-01-06 14:22:15 +0545

    Initial commit: basic Python practice codes

---
+++ Simple calculator.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number"))
+b= int(input("Enter second number"))
+sum = a+b
+dif = a-b
+div =a/b
+mul = a*b
+print("Sum is:",sum)
+print("Difference is:",dif)
+print("Division is:",div)   
+print("Multiplication is:",mul)
