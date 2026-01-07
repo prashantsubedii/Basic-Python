574f77e2bc9d419d1a03cab4e5b9b0a08e029728
Prashant Subedi <179603709+prashantsubedii@users.noreply.github.com>
2026-01-07 17:59:28 +0545

    Strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+str1= "This is a string"
+print(str1)
+
+
+#Concatenation
+
+str2="Hello"
+str3="\n World!"
+str4=str2+" "+str3
+print(str4)
+
+#functions
+
+len1=len(str4)
+print("Length of str4 is:",len1)
+print(str4[3])
+print(str4[0:5])
+print("This",str4.lower())
+
+print("This",str4.upper())
+
