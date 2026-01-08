32afc559ad8b06c8c4e59b6465fe8d9e21b2468b
Prashant Subedi <179603709+prashantsubedii@users.noreply.github.com>
2026-01-08 20:30:37 +0545

    lists and tuples

---
+++ lists and tuples.py
@@ -0,0 +1,26 @@
+#List
+
+marks = [90, 85, 78, 92, 88]
+print("Original List:", marks)
+print(type(marks))
+print("Length of List:", len(marks))
+marks[3] = 95
+print("Updated List:", marks)
+print(marks[3])
+stringlist = ["apple", "banana", "cherry", 98,99]
+print("String List:", stringlist)
+slice=['hello',5,5,5,5,5]
+print(slice[-0:-1])
+
+
+#Tuple
+
+tup=()
+print(type(tup))
+tup1=(1,2,3,4,5,5,5,5,5)
+print("Tuple 1:", tup1) 
+print(tup1.index(3))
+print("Count of 5 in Tuple 1:", tup1.count(5))
+
+
+
