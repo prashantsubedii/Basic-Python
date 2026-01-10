41cfdc0654aed99cfacb83f987103a4ab1907e5c
Prashant Subedi <179603709+prashantsubedii@users.noreply.github.com>
2026-01-10 20:08:33 +0545

    commit loops

---
+++ loops.py
@@ -0,0 +1,15 @@
+print('enter value of n')
+n=int(input())
+sum=0
+for i in range(1,n+1):
+    sum = sum + i
+print('The sum is:',sum)
+
+
+#while
+
+print('enter value of num')
+num=int(input())
+while(num>=0):
+    print('value of num:',num)
+    num= num-1
