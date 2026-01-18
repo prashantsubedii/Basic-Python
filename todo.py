fe97a0e1aad9d552a89faab44d079eb3ae9af610
Prashant Subedi <179603709+prashantsubedii@users.noreply.github.com>
2026-01-18 18:59:02 +0545

    Todo update

--- todo.py
+++ todo.py
@@ -1,15 +1,15 @@
-tasks = []
+tasks = [] #list to store tasks
 
 while True:
     print("1.Add 2.View 3.Delete 4.Exit")
-    ch = int(input("Choose: "))
+    ch = int(input("Choose: ")) #user choice
 
     if ch == 1:
         task = input("Enter task: ")
-        tasks.append(task)
+        tasks.append(task) #add task to the list
     elif ch == 2:
         print(tasks)
     elif ch == 3:
-        tasks.pop()
+        tasks.pop() #remove last task from the list
     elif ch == 4:
-        break
+        break #exit the loop
